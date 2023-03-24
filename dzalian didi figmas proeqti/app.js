a9faf011985e4ea26d11b7a311b581019a35e425
lado311 <ladunaokruashvili.44@gmail.com>
2023-03-24 14:02:16 -0700

    figma commit

---
+++ dzalian didi figmas proeqti/app.js
@@ -0,0 +1,14 @@
+let hamburger = document.querySelector('.hamburger');
+let navmenu = document.querySelector('.nav-menu');
+let phonemenu = document.querySelector('.nav-img-box2');
+
+
+hamburger.addEventListener('click' , ()=>{
+    hamburger.classList.toggle('active');
+    navmenu.classList.toggle('active');
+    phonemenu.classList.toggle('active');
+
+})
+
+//აქ გავაკეთე ბურგერ მენიუ//
+
